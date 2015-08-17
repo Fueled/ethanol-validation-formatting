b15d5714092d4653c956afaea0477e86594d1310
stephane-fueled <stephane@fueled.com>
2015-08-17 15:28:57 -0400

    chore(comments): add comments on credit card enumeration

--- EthanolValidationFormatting/NSString+CreditCard.h
+++ EthanolValidationFormatting/NSString+CreditCard.h
@@ -8,12 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Define the types of credit card that the property below can detect
+ */
 typedef NS_ENUM(NSInteger, ETHCreditCardType) {
+  /**
+   *  An unknown credit card type (Any sequence of number between 6 and 19 characters that doesn't fit the others)
+   */
   ETHCreditCardTypeUnknown,
+  /**
+   *  American Express
+   */
   ETHCreditCardTypeAmex,
+  /**
+   *  Visa
+   */
   ETHCreditCardTypeVisa,
+  /**
+   *  Master Card
+   */
   ETHCreditCardTypeMastercard,
+  /**
+   *  Discover
+   */
   ETHCreditCardTypeDiscover,
+  /**
+   *  Anything that is not a sequence of number, or that is lesser than 6 characters/greater than 19 characters
+   */
   ETHCreditCardTypeNotACreditCard
 };
 
