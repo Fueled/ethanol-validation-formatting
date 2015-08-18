fc105221ee20f9eaf7c42c66cdc0169a5a5cff11
stephane-fueled <stephane@fueled.com>
2015-08-18 15:56:16 -0400

    fix(tests): fix unit tests configuration

---
+++ EthanolValidationFormattingTests/FormattersTests.m
@@ -0,0 +1,26 @@
+//
+//  FormattersTests.m
+//  EthanolValidationFormattingTests
+//
+//  Created by Stephane Copin on 7/1/15.
+//  Copyright © 2015 Stephane Copin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+@interface FormattersTests : XCTestCase
+
+@end
+
+@implementation FormattersTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+@end
